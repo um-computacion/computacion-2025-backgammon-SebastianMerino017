db2ba70b04d47c2ba2745d72b249ffbd354fa54d
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-09-04 22:08:19 -0300

    test valores maximos, minimos, normales y llamadas consecutivas

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -2,13 +2,7 @@ from core.dice import get_dice
 from unittest import TestCase
 from unittest.mock import patch
 
-
 class TestDice(TestCase):
-
-    # @patch.object(Clase, 'metodo', side_effect=[5, 2])
-    # def test_method(self, randint_patched):
-    #     ...
-
     @patch('random.randint', side_effect=[5, 2])
     def test_simple(self, randint_patched):
         dice = get_dice()
@@ -36,7 +30,6 @@ class TestDice(TestCase):
         self.assertTrue(randint_patched.called)
         self.assertEqual(randint_patched.call_count, 1)
 
-
     def test_double(self):
         with patch('random.randint', side_effect=[5, 2]) as randint_patched:
             dice = get_dice()
@@ -46,12 +39,45 @@ class TestDice(TestCase):
             self.assertTrue(randint_patched.called)
             self.assertEqual(randint_patched.call_count, 2)
 
-        with patch('random.randint', return_value=1) as randint_patched:
-            dice = get_dice()
-            self.assertEqual(len(dice), 4)
-            self.assertEqual(dice[0], 1)
-            self.assertEqual(dice[1], 1)
-            self.assertEqual(dice[2], 1)
-            self.assertEqual(dice[3], 1)
+    @patch('random.randint', side_effect=[6, 6])
+    def test_max_values(self, randint_patched):
+        dice = get_dice()
+        self.assertEqual(len(dice), 2)
+        self.assertEqual(dice[0], 6)
+        self.assertEqual(dice[1], 6)
+        self.assertTrue(randint_patched.called)
+        self.assertEqual(randint_patched.call_count, 2)
+
+    @patch('random.randint', side_effect=[1, 1])
+    def test_min_values(self, randint_patched):
+        dice = get_dice()
+        self.assertEqual(len(dice), 4)
+        self.assertEqual(dice[0], 1)
+        self.assertEqual(dice[1], 1)
+        self.assertEqual(dice[2], 1)
+        self.assertEqual(dice[3], 1)
+        self.assertTrue(randint_patched.called)
+        self.assertEqual(randint_patched.call_count, 2)
+
+    @patch('random.randint', side_effect=[3, 4])
+    def test_normal_values(self, randint_patched):
+        dice = get_dice()
+        self.assertEqual(len(dice), 2)
+        self.assertEqual(dice[0], 3)
+        self.assertEqual(dice[1], 4)
+        self.assertTrue(randint_patched.called)
+        self.assertEqual(randint_patched.call_count, 2)
+
+    def test_multiple_calls(self):
+        with patch('random.randint', side_effect=[2, 3, 5, 1]) as randint_patched:
+            dice1 = get_dice()
+            dice2 = get_dice()
+            self.assertEqual(len(dice1), 2)
+            self.assertEqual(len(dice2), 2)
+            self.assertEqual(dice1[0], 2)
+            self.assertEqual(dice1[1], 3)
+            self.assertEqual(dice2[0], 5)
+            self.assertEqual(dice2[1], 1)
             self.assertTrue(randint_patched.called)
-            self.assertEqual(randint_patched.call_count, 2)
+            self.assertEqual(randint_patched.call_count, 4)
+    
