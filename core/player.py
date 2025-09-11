5c451ff2b36a46c3fe88ad25f2a1cde6e240f097
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-09-10 21:55:30 -0300

    comienzo estructura nueva

--- core/player.py
+++ core/player.py
@@ -1,4 +1,17 @@
 import random
 
 class Player:
- 
+    current_turn = None
+    turn_counter = 0
+    game_pieces = {
+        'white': {'on_board': 15, 'off_board': 0},
+        'black': {'on_board': 15, 'off_board': 0}
+    }
+
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        self.score = 0
+
+        if Player.current_turn is None: 
+            Player.current_turn = "white"
