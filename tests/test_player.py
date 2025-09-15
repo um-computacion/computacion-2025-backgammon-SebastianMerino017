9bd4ed4e81449f8fc29b6be003f45bb75db9244c
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-09-15 12:48:42 -0300

    correccion clase player, y testeo de turnos y dados

--- tests/test_player.py
+++ tests/test_player.py
@@ -17,3 +17,17 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.p1.color, "white")
         self.assertEqual(Player.current_turn, "white")
         self.assertEqual(Player.game_pieces['white']['on_board'], 15)
+
+    def test_turns(self):
+        self.assertTrue(self.p1.is_my_turn())
+        self.p1.end_turn()
+        self.assertTrue(self.p2.is_my_turn())
+    
+    def test_roll_dice_valid_turn(self):
+        dice = self.p1.roll_dice()
+        self.assertEqual(len(dice), 2)
+        for die in dice:
+            self.assertIn(die, range(1, 7))
+    
+    def test_roll_dice_invalid_turn(self):
+        self.assertIsNone(self.p2.roll_dice())
