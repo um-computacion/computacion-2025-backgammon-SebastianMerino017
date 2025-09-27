099dbc54ef372c56903440e48dca46dead445bcc
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-09-27 20:53:41 -0300

    configuracion de fallos

--- core/board.py
+++ core/board.py
@@ -2,5 +2,23 @@ class Board:
     def __init__(self):
         self.positions = {}
         self.bar = {"white": 0, "black": 0}
+        self.off_board = {"white": 0, "black": 0}
         self.setup_initial_positions()
 
+    def setup_initial_position(self):
+        self.pos = [None for _ in range(24)]
+        
+        self.pos[0] = ["white", 2]
+        self.pos[11] = ["white", 5]
+        self.pos[16] = ["white", 3]
+        self.pos[18] = ["white", 5]
+        
+        self.pos[23] = ["black", 2]
+        self.pos[12] = ["black", 5]
+        self.pos[7] = ["black", 3]
+        self.pos[5] = ["black", 5]
+
+
+    
+
+    
