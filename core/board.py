3dd24514ef2ae00776365fe4cb4e3f7da25940fa
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-09-27 21:08:58 -0300

    clase board con representacion visual y manejo de fichas

--- core/board.py
+++ core/board.py
@@ -19,6 +19,28 @@ class Board:
         self.pos[5] = ["black", 5]
 
 
-    
+    def draw(self):
+        result_board = [] 
+        for col in range(11, -1, -1):
+            result_row = []
+            result_board.append(result_row)
+            for row in range(0, 5):
+                if self.pos[col] is not None:
+                    if self.pos[col][1] > row:
+                        if row < 4:
+                            piece = self.get_piece(col)
+                        else:
+                            if self.pos[col][1] <= 5:
+                                piece = self.get_piece(col)
+                            else:
+                                piece = str(self.pos[col][1] - 4)
+                        result_row.append(piece)
+                    else:
+                        result_row.append(' ')    
+                else:
+                    result_row.append(' ')
+        return result_board
+
+
 
     
