1fd3845b2857036611f1e02f5a26ca52ed56f795
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-10-07 17:31:12 -0300

    arreglo de estructura de test

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,12 +3,16 @@ from core.board import Board
 
 
 class TestBoard(unittest.TestCase):
-    def test_draw(self):
-        board = Board()
-        board.pos[0] = ('white', 3)
-        board.pos[1] = ('white', 8)
-        board.pos[23] = ('black', 1)
-        board.pos[22] = ('black', 3)
-        print(board.draw())
-        board_draw = board.draw()
-        print(f"board 0, 11: {board_draw[11][0]}")
+   def test_draw(self):
+    board = Board()
+    board.pos = [None for _ in range(24)]
+    board.pos[0] = ["white", 3]
+    board.pos[1] = ["white", 8]
+    
+    board_draw = board.draw()
+    
+    self.assertEqual(len(board_draw), 12)
+    
+    self.assertEqual(board_draw[11][0], 'W')
+    self.assertEqual(board_draw[11][1], 'W')
+    self.assertEqual(board_draw[11][2], 'W')
