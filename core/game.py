24c58d6adb5019f18b5a33feb6028116532b36b6
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-10-12 11:02:56 -0300

    agregar metodos de acceso y logica de turno

--- core/game.py
+++ core/game.py
@@ -30,3 +30,37 @@ class Game:
     def start(self):
         self.__game_started__ = True
         return True
+    
+    def get_current_player(self):
+        return self.__current_player__
+    
+    def get_board(self):
+        return self.__board__
+    
+    def get_dice(self):
+        return self.__dice__
+    
+    def get_players(self):
+        return (self.__player1__, self.__player2__)
+    
+    def roll_dice(self):
+        if not self.__game_started__:
+            return None
+        
+        current = self.get_current_player()
+        if not current.is_my_turn():
+            raise NotYourTurnError(f"No es el turno de {current.name}")
+        
+        result = self.__dice__.roll()
+        return result
+    
+    def get_available_moves(self):
+        return self.__dice__.get_available_values()
+    
+    def must_enter_from_bar(self):
+        current = self.get_current_player()
+        return self.__board__.__bar__[current.color] > 0
+    
+    def can_bear_off(self):
+        current = self.get_current_player()
+        return self.__board__.has_pieces_in_home_board(current.color)
